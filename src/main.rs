cc880cadb984bd8cc1dbb2a7a387259b8a5da391
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2026-03-07 20:19:05 +0900

    fmt
    Signed-off-by: Seonghyeon Cho <seonghyeoncho96@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,7 @@ fn main() -> ExitCode {
         Commands::Compare(args) => {
             commands::compare::execute(args, cli.json, cli.pretty, cli.no_color)
         }
-        Commands::Sort(args) => {
-            commands::sort::execute(args, cli.json, cli.pretty, cli.no_color)
-        }
+        Commands::Sort(args) => commands::sort::execute(args, cli.json, cli.pretty, cli.no_color),
         Commands::Info(args) => commands::info::execute(args, cli.json, cli.pretty, cli.no_color),
         Commands::Completions(args) => commands::completions::execute(args),
     };
